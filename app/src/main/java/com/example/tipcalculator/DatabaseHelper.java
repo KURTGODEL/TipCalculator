aea9b1a81b4aef6b3d6fd45aa510126743594dca
KURTGODEL <erickcpgomes@gmail.com>
2020-01-23 10:02:32 -0500

    Implementing new Activity #3 and #4

--- app/src/main/java/com/example/tipcalculator/DatabaseHelper.java
+++ app/src/main/java/com/example/tipcalculator/DatabaseHelper.java
@@ -1,6 +1,8 @@
 package com.example.tipcalculator;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -17,7 +19,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
+
     }
 
     @Override
@@ -34,5 +36,27 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean insertData(String name, String surname, String marks){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(NAME, name);
+        contentValues.put(SURNAME, surname);
+        contentValues.put(MARKS, marks);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if(result == -1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
     }
 }
--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -1,19 +1,20 @@
 package com.example.tipcalculator;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.database.sqlite.SQLiteDatabase;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.math.BigDecimal;
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView lblAmount;
     private Double billAmount;
     private TextView lblTest;
+
+    /* ********* ACTIVITY #3 ********* */
+    private EditText editTextName, editTextSurname, editTextMarks;
+    private Button buttonAddData;
+    private Button buttonViewAll;
 
     private static final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
 
@@ -44,23 +50,30 @@ public class MainActivity extends AppCompatActivity {
         txtTotal = findViewById(R.id.txtTotal);
         lblSeekBar = findViewById(R.id.lblSeekBar);
         lblAmount = findViewById(R.id.lblAmount);
-        lblTest = findViewById(R.id.lblTest);
+        lblTest = findViewById(R.id.txtView3);
+
+        /* ********* ACTIVITY #3 ********* */
+        editTextName = findViewById(R.id.editTextName);
+        editTextSurname = findViewById(R.id.editTextSurname);
+        editTextMarks = findViewById(R.id.editTextMarks);
+        buttonAddData = findViewById(R.id.button_add);
+        buttonViewAll = findViewById(R.id.button_viewall);
 
 
         /* ********* ACTIVITY #1 ********* */
-        DBHelper dbHelper = new DBHelper(this);
-        dbHelper.insertUsers();
+        //DBHelper dbHelper = new DBHelper(this);
+        //dbHelper.insertUsers();
 
-        lblTest.setText(String.valueOf(dbHelper.numberOfRows()));
+        //lblTest.setText(String.valueOf(dbHelper.numberOfRows()));
 
-        lblTest.setText(dbHelper.getUser(1));
+        //lblTest.setText(dbHelper.getUser(1));
 
 
 
         /* ********* ACTIVITY #2 ********* */
         myDB = new DatabaseHelper(this);
-
-
+        AddData();
+        ViewAll();
 
 
 
@@ -105,6 +118,68 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    /* ********* ACTIVITY #3 ********* */
+    public void AddData(){
+        buttonAddData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isInserted = myDB.insertData(
+                        editTextName.getText().toString(),
+                        editTextSurname.getText().toString(),
+                        editTextMarks.getText().toString());
+
+                if(isInserted){
+                    Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Data Not Inserted", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    /* ********* ACTIVITY #3 ********* */
+    public void ViewAll(){
+        buttonViewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor res = myDB.getAllData();
+
+                if(res.getCount() == 0){
+                    showMessage("ALERT", "No data found");
+                    return;
+                }
+
+                StringBuffer buffer = new StringBuffer();
+
+                while(res.moveToNext()){
+                    buffer.append("Id: " + res.getString(0) + "\t ");
+                    buffer.append("Name: " + res.getString(1)+ "\t ");
+                    buffer.append("Surname: " + res.getString(2)+ "\t ");
+                    buffer.append("Marks: " + res.getString(3)+ "\n\n");
+                }
+
+                showMessage("DATA", buffer.toString());
+            }
+        });
+    }
+
+    /* ********* ACTIVITY #3 ********* */
+    public void showMessage(String title, String message){
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.scrollable, null);
+
+        TextView textview = (TextView) view.findViewById(R.id.textmsg);
+        textview.setText(message);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setView(view);
+        builder.setTitle(title);
+
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
     private void calculateTipTotal(){
